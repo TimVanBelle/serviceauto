d630e07fbff55d6c30b470498688b6e6014967d6
flovck <fvonck@norauto.fr>
2016-03-15 15:17:53 +0100

    controller et view groom

---
+++ app/controllers/grooms_controller.rb
@@ -0,0 +1,41 @@
+class GroomController < ApplicationController
+
+  def new
+    @groom = Groom.new
+  end
+
+  def create
+    @groom = Groom.new(lift_params)
+    @groom.user = current_user
+
+    if @groom.save
+      redirect_to @groom
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @groom = Groom.new
+  end
+
+  def show
+    @groom = Groom.find(params[:id])
+  end
+
+  def update
+     @groom = Groom.new(groom_params)
+    if @groom.save
+     redirect_to groom_path(@groom)
+    else
+     render :edit
+    end
+  end
+
+  private
+
+  def groom_params
+    params.require(:groom).permit(:firt_name, :last_name, :mail, :phone_number, :driving_licence, :rib, :address, :picture, :picture_cache)
+  end
+
+end
