d0c2862e51110834c3240169c35a2c4a95320768
Timothee Vanbelle <timothee_vanbelle@hotmail.fr>
2016-03-24 17:57:44 +0100

    modifier couleur de comment ça marche

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
 
   resources :cars, only: [:new, :create, :edit, :update, :destroy]
 
+
   # resources :order_items, only: [:create, :update, :destroy]
   #
   # get 'services/index'
